f2a75ab5f02745cd52809ebd83ac6fd6742e60f2
Joshua Seigler <seigler@users.noreply.github.com>
2019-11-29 14:32:55 -0500

    Chore: rename bundle to lib/dash.min.js Change library reference in ES5 example JS to point to bundle Change gitignore and webpack dist -> lib

--- examples/usage.es6.ts
+++ examples/usage.es6.ts
@@ -10,12 +10,14 @@ const opts: SDKOpts = {
 };
 const sdk = new DashJS.SDK(opts);
 const acc = sdk.wallet.getAccount();
+readDocument();
+
 async function sendPayment(){
     const tx = await acc.createTransaction({recipient:{address:'yLptqWxjgTxtwKJuLHoGY222NnoeqYuN8h', amount:0.12}})
     console.log(tx)
 }
 
 async function readDocument() {
-    const profile = await sdk.platform.fetchDocuments('profile',  opts)
+    const profile = await sdk.platform.documents.fetch('profile',  opts)
     console.log(profile);
 }
