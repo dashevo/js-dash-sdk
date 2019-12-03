11b80a2c92d61653b8de9c8921264a1f919efd45
Alex Werner <alex@werner.fr>
2019-12-03 03:17:05 +0100

    fix(usage.es6):  ts compilation error

--- examples/usage.es6.ts
+++ examples/usage.es6.ts
@@ -6,18 +6,24 @@ const network = "testnet";
 const opts: SDKOpts = {
     network,
     mnemonic: "arena light cheap control apple buffalo indicate rare motor valid accident isolate",
-    schema
+    schemas: {
+        dashpay: schema
+    }
 };
 const sdk = new DashJS.SDK(opts);
-const acc = sdk.wallet.getAccount();
+const {wallet, platform} = sdk;
+
 readDocument();
 
-async function sendPayment(){
-    const tx = await acc.createTransaction({recipient:{address:'yLptqWxjgTxtwKJuLHoGY222NnoeqYuN8h', amount:0.12}})
+async function sendPayment() {
+    if (!wallet) throw new Error('Missing wallet');
+    const acc = wallet.getAccount();
+    const tx = await acc.createTransaction({recipient: {address: 'yLptqWxjgTxtwKJuLHoGY222NnoeqYuN8h', amount: 0.12}})
     console.log(tx)
 }
 
 async function readDocument() {
-    const profile = await sdk.platform.documents.fetch('profile',  opts)
+    if (platform===undefined) throw new Error('Missing platform');
+    const profile = await platform.documents.fetch('dashpay.profile', opts)
     console.log(profile);
 }
