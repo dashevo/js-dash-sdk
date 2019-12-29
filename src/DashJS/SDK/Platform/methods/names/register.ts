06bc9352f6456d245506b18f24d0c4c09a6a7df8
Alex Werner <alex@werner.fr>
2019-12-29 22:30:29 +0100

    doc: removed unused Identity dep

--- src/DashJS/SDK/Platform/methods/names/register.ts
+++ src/DashJS/SDK/Platform/methods/names/register.ts
@@ -1,4 +1,3 @@
-import {Identity} from '@dashevo/dpp/lib/identity/Identity';
 import {Platform} from "../../Platform";
 
 export async function register(this: Platform, identityType: string = 'USER', identity: any): Promise<any> {
