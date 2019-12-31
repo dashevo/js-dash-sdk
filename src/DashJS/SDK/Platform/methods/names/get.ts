0479371f0ced4e04d7193c407829ce63f72fe9be
Alex Werner <alex@werner.fr>
2019-12-31 02:36:37 +0100

    style: names.get style with queryOpts

--- src/DashJS/SDK/Platform/methods/names/get.ts
+++ src/DashJS/SDK/Platform/methods/names/get.ts
@@ -1,9 +1,14 @@
 import {Platform} from "../../Platform";
 
 export async function get(this: Platform, id: string): Promise<any> {
-    //FIXME : Where query has an issue here.
-    // const documents = await this.documents.get('dpns.domain', { where: [[`data.label`, '==', id]] });
-    const documents = await this.documents.get('dpns.domain', {});
+    //FIXME : Where query have an issue here failing with Error 3.
+    // const queryOpts = {
+    //     where: [
+    //         ['label', '==', id.toLowerCase()],
+    //     ],
+    // }
+    const queryOpts = {};
+    const documents = await this.documents.get('dpns.domain', queryOpts);
 
     const names = documents.filter((el: { data: { label: string; }; }) => el.data.label === id);
     return names[0] || {};
