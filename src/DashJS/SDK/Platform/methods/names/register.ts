f594bccdd77672a6fc854ec01761dea9163e7f71
Alex Werner <alex@werner.fr>
2020-01-17 14:16:04 +0100

    fix(DPNS): use local document.create method

--- src/DashJS/SDK/Platform/methods/names/register.ts
+++ src/DashJS/SDK/Platform/methods/names/register.ts
@@ -45,15 +45,10 @@ export async function register(this: Platform,
     if(!this.apps.dpns.contractId){
         throw new Error('DPNS is required to register a new name.');
     }
-    const dataContract = await this.contracts.get(this.apps.dpns.contractId);
-    if(!dataContract){
-        throw new Error('DPNS Contract not loaded. Cannot register.');
-    }
     // 1. Create preorder document
-    const preorderDocument = dpp.document.create(
-        dataContract,
-        identity.id,
+    const preorderDocument = this.documents.create(
         'dpns.preorder',
+        identity,
         {
             saltedDomainHash,
         },
@@ -66,10 +61,9 @@ export async function register(this: Platform,
     await client.applyStateTransition(preorderTransition);
 
     // 3. Create domain document
-    const domainDocument = dpp.document.create(
-        dataContract,
-        identity.id,
+    const domainDocument = this.documents.create(
         'dpns.domain',
+        identity,
         {
             nameHash,
             label,
