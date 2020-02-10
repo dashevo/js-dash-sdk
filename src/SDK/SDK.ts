45311eb808261b504842f0d0d5e421d09e0b5bbb
Alex Werner <alex@werner.fr>
2020-02-10 09:36:28 +0100

    feat: added wallet-lib + dapi-client + dpp primitives

--- src/SDK/SDK.ts
+++ src/SDK/SDK.ts
@@ -1,4 +1,13 @@
 import {Client as _Client} from './Client';
+import {default as _DAPIClient} from '@dashevo/dapi-client';
+// @ts-ignore
+import {default as _DashPlatformProtocol} from '@dashevo/dpp';
+import {
+    Wallet as _Wallet,
+    Account as _Account,
+    KeyChain as _KeyChain,
+} from '@dashevo/wallet-lib';
+
 import {
     Transaction as _Transaction,
     Address as _Address,
@@ -17,6 +26,14 @@ import {
 
 export namespace SDK {
     export let Client = _Client;
+
+    export let DAPIClient = _DAPIClient;
+    export let DashPlatformProtocol = _DashPlatformProtocol;
+
+    // Wallet-lib primitives
+    export let Wallet = _Wallet;
+    export let Account = _Account;
+    export let KeyChain = _KeyChain;
 
     // Dashcore-lib primitives
     export let Transaction = _Transaction;
