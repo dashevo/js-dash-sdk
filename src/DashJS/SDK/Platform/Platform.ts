307ff39a1894311a4b6719b6d1f1f326398c5d72
Steve Huguenin-Elie <steve.huguenin-elie@protonmail.ch>
2020-02-15 14:11:29 +0100

    implemented ts-doc =>     - enables intellisense in modern editors     - API documentation can be exported

--- src/DashJS/SDK/Platform/Platform.ts
+++ src/DashJS/SDK/Platform/Platform.ts
@@ -22,6 +22,13 @@ import registerName from "./methods/names/register";
 // @ts-ignore
 import {Account} from "@dashevo/wallet-lib";
 
+/**
+ * Interface for PlatformOpts
+ * 
+ * @remarks
+ * required parameters include { client, apps }
+ * optional parameters include { ..., account?, network? }
+ */
 export interface PlatformOpts {
     client: DAPIClient,
     apps: SDKApps
@@ -29,22 +36,47 @@ export interface PlatformOpts {
     network?: string
 }
 
-
+/**
+ * Class for Dash Platform
+ * 
+ * @param documents - documents
+ * @param identities - identites
+ * @param names - names
+ * @param contracts - contracts
+ */
 export class Platform {
     dpp: DashPlatformProtocol;
+    /**
+     * @param {Function} broadcast - broadcast documents onto the platform
+     * @param {Function} create - create documents which can be broadcasted
+     * @param {Function} get - get documents from the platform
+     */
     public documents: {
         broadcast:Function,
         create:Function,
         get:Function,
     };
+    /**
+     * @param {Function} get - get identities from the platform
+     * @param {Function} register - register identities on the platform
+     */
     public identities: {
         get:Function,
         register:Function,
     };
+    /**
+     * @param {Function} get - get names from the platform
+     * @param {Function} register - register names on the platform
+     */
     public names: {
         get:Function,
         register:Function,
     };
+    /**
+     * @param {Function} get - get contracts from the platform
+     * @param {Function} create - create contracts which can be broadcasted
+     * @param {Function} register - register contracts on the platform
+     */
     public contracts: {
         broadcast:Function,
         create:Function,
@@ -55,6 +87,11 @@ export class Platform {
     account?: Account;
     network?: string;
 
+    /**
+     * Construct some instance of Platform
+     * 
+     * @param {platformOpts} - options for Platform
+     */
     constructor(platformOpts: PlatformOpts) {
         this.documents = {
             broadcast: broadcastDocument.bind(this),
