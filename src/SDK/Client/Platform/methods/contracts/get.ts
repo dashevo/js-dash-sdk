15f256c0014222c23a72ec77d4997f031c06db89
Alex Werner <alex@werner.fr>
2020-06-01 19:45:40 +0200

    perf: remove unnecessary and performance costly try/catch

--- src/SDK/Client/Platform/methods/contracts/get.ts
+++ src/SDK/Client/Platform/methods/contracts/get.ts
@@ -24,24 +24,19 @@ export async function get(this: Platform, identifier: ContractIdentifier): Promi
     if (localContract && localContract.contract) {
         return localContract.contract;
     } else {
-        try {
-            const rawContract = await dapiClient.getDataContract(identifier);
-            if(!rawContract){
-                return null;
-            }
-
-            const contract = await this.dpp.dataContract.createFromSerialized(rawContract);
-            const app = {contractId: identifier, contract};
-
-            // If we do not have even the identifier in this.apps, we add it with timestamp as key
-            if (localContract === undefined || !localContract.contract) {
-                this.apps[Date.now()] = app
-            }
-            return app.contract;
-        } catch (e) {
-            throw e;
+        const rawContract = await dapiClient.getDataContract(identifier);
+        if(!rawContract){
+            return null;
         }
 
+        const contract = await this.dpp.dataContract.createFromSerialized(rawContract);
+        const app = {contractId: identifier, contract};
+
+        // If we do not have even the identifier in this.apps, we add it with timestamp as key
+        if (localContract === undefined || !localContract.contract) {
+            this.apps[Date.now()] = app
+        }
+        return app.contract;
     }
 }
 
