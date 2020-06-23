473bf5020b9557d932bc3a9a9aba2d4393d5472d
Djavid Gabibiyan <djavidoff@gmail.com>
2020-06-23 11:45:52 +0500

    chore: finalize `register` unit test

--- src/SDK/Client/Platform/methods/names/register.spec.ts
+++ src/SDK/Client/Platform/methods/names/register.spec.ts
@@ -1,4 +1,9 @@
 import { expect } from 'chai';
+import { ImportMock } from 'ts-mock-imports';
+
+import entropyModule from '@dashevo/dpp/lib/util/entropy';
+
+ImportMock.mockFunction(entropyModule, 'generate', 'someEntropy');
 
 import register from './register';
 
@@ -28,7 +33,9 @@ describe('Platform', () => {
             });
 
             it('register top level domain', async () => {
-                await register.call(platformMock, 'dash', identityMock);
+                identityMock.getId.returns('someIdentityId');
+
+                await register.call(platformMock, 'Dash', identityMock);
 
                 // @ts-ignore
                 expect(identityMock.getId).to.have.been.calledOnce();
@@ -36,13 +43,55 @@ describe('Platform', () => {
                     'dpns.preorder',
                     identityMock,
                     {
-                        "saltedDomainHash": "56206d7ec5b71859eed1fb91398efc440c22f8121426beefa4f7542762a636d83e38",
+                        "saltedDomainHash": "5620d033a9faaa2633afd855570b28b11190a5f2d16634021eb0acf8cee7d402b756",
                     },
+                ]);
+
+                expect(platformMock.documents.create.getCall(1).args).to.have.deep.members([
+                    'dpns.domain',
+                    identityMock,
+                    {
+                        'label': 'Dash',
+                        'nameHash': '562060f0833932a21446ada9b0bb71ac8e8b40e2618f99f44204d66815f6bdf258cc',
+                        'normalizedLabel': 'dash',
+                        'normalizedParentDomainName': '',
+                        'preorderSalt': 'someEntropy',
+                        'records': {
+                            'dashIdentity': 'someIdentityId',
+                        }
+                    }
                 ]);
             });
 
             it('should register second level domain', async () => {
-                await register.call(platformMock, 'user.dash', identityMock);
+                identityMock.getId.returns('someIdentityId');
+
+                await register.call(platformMock, 'User.dash', identityMock);
+
+                // @ts-ignore
+                expect(identityMock.getId).to.have.been.calledOnce();
+                expect(platformMock.documents.create.getCall(0).args).to.have.deep.members([
+                    'dpns.preorder',
+                    identityMock,
+                    {
+                        "saltedDomainHash": "5620ca5eed7648dcb77804e80c17753c23b1b77d43ca9ead90dee01c9c913ca4f13e",
+                    },
+                ]);
+
+                expect(platformMock.documents.create.getCall(1).args).to.have.deep.members([
+                    'dpns.domain',
+                    identityMock,
+                    {
+                        'label': 'User',
+                        'nameHash': '5620b5f42fb635a08cc0f441bbc6ef5f3bdeed2877692feffd9945bde3abf8b4141f',
+                        'normalizedLabel': 'user',
+                        'normalizedParentDomainName': 'dash',
+                        'preorderSalt': 'someEntropy',
+                        'records': {
+                            'dashIdentity': 'someIdentityId',
+                        }
+                    }
+                ]);
             });
 
             it('should fail if DPNS app have no contract set up', async () => {
