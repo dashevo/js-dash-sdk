57115b17afd16797c6ee1ccf36686a247f353101
Djavid Gabibiyan <djavidoff@gmail.com>
2021-06-30 14:28:16 +0500

    chore: set meta

--- src/SDK/Client/Platform/methods/identities/get.ts
+++ src/SDK/Client/Platform/methods/identities/get.ts
@@ -1,6 +1,7 @@
 import {Platform} from "../../Platform";
 // @ts-ignore
 import Identifier from "@dashevo/dpp/lib/Identifier";
+import Metadata from "@dashevo/dpp/lib/Metadata";
 
 /**
  * Get an identity from the platform
@@ -15,13 +16,27 @@ export async function get(this: Platform, id: Identifier|string): Promise<any> {
     const identifier = Identifier.from(id);
 
     // @ts-ignore
-    const identityBuffer = await this.client.getDAPIClient().platform.getIdentity(identifier);
+    const identityResponse = await this.client.getDAPIClient().platform.getIdentity(identifier);
+
+    const identityBuffer = identityResponse.getIdentity();
 
     if (identityBuffer === null) {
         return null;
     }
 
-    return this.dpp.identity.createFromBuffer(identityBuffer);
+    const identity = this.dpp.identity.createFromBuffer(identityBuffer);
+
+    let metadata = null;
+    const responseMetadata = identityResponse.getMetadata();
+    if (responseMetadata) {
+        metadata = new Metadata({
+            blockHeight: responseMetadata.getHeight(),
+            coreChainLockedHeight: responseMetadata.getCoreChainLockedHeight(),
+        });
+    }
+    identity.setMetadata(metadata);
+
+    return identity;
 }
 
 export default get;
