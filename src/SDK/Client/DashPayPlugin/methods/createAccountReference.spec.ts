67c4457a54d8ebba85b7e4f3e160fd6f48f7b94b
Alex Werner <alex@werner.fr>
2021-09-13 20:24:26 +0200

    fix: createAccountReference 28 MSB

--- src/SDK/Client/DashPayPlugin/methods/createAccountReference.spec.ts
+++ src/SDK/Client/DashPayPlugin/methods/createAccountReference.spec.ts
@@ -3,12 +3,15 @@ import { expect } from 'chai';
 import { createAccountReference } from "./createAccountReference";
 import { HDPublicKey, PrivateKey } from "@dashevo/dashcore-lib";
 
+const senderPrivateKey1 = new PrivateKey('2fc4145c8b7a871c42e32733a83c36f9b0d0eb646f40e53cb9ae0f48669ab0d7');
+const extendedPublicKey1 = new HDPublicKey('tpubDMDatc2kPVD8R6hW2gQnmNDJ4xANWYueibmhPJoRnCjnagrTrRdFCDrzwD4bWaacsL4mms8dRyvWNLtzFYCuguTcXQRbiza1FnnFKT21GC6');
+
+const extendedPublicKey2 = new HDPublicKey('tpubDLBwGAKsAffoGpUhHcceos8kkJh9ZTRSkjEm25ZbCrQMK9d68hi2yDjkWfCbdgaugmRrTxWQDRUd8Mb6SiXKCeosaSKT5piAhr7emPABFwJ');
+
 describe('DashPayPlugin - createAccountReference', () => {
     it('create an account reference', function () {
-        const senderPrivateKey = new PrivateKey('2fc4145c8b7a871c42e32733a83c36f9b0d0eb646f40e53cb9ae0f48669ab0d7');
-        const extendedPublicKey = new HDPublicKey('tpubDMDatc2kPVD8R6hW2gQnmNDJ4xANWYueibmhPJoRnCjnagrTrRdFCDrzwD4bWaacsL4mms8dRyvWNLtzFYCuguTcXQRbiza1FnnFKT21GC6');
-
-        expect(createAccountReference(senderPrivateKey.toBuffer(), extendedPublicKey.toBuffer())).to.deep.equal(93353124);
+        expect(createAccountReference(senderPrivateKey1.toBuffer(), extendedPublicKey1.toBuffer())).to.deep.equal(70557813);
+        expect(createAccountReference(senderPrivateKey1.toBuffer(), extendedPublicKey2.toBuffer())).to.deep.equal(118374606);
     });
 
 });
--- src/SDK/Client/DashPayPlugin/methods/createAccountReference.ts
+++ src/SDK/Client/DashPayPlugin/methods/createAccountReference.ts
@@ -8,13 +8,13 @@ import { sha256hmac } from "@dashevo/dashcore-lib/lib/crypto/hash"
  * @param accountIndex
  * @param version
  */
-export function createAccountReference(senderPrivateKeyBuffer, extendedPublicKeyBuffer, accountIndex = 0, version = 0){
-    const AccountSecretKeyBuffer = sha256hmac(senderPrivateKeyBuffer, extendedPublicKeyBuffer);
-    const AccountSecretKey32Buffer = AccountSecretKeyBuffer.slice(0, 4);
-    const AccountSecretKey28Buffer = parseInt(AccountSecretKey32Buffer.toString('hex'), 16) >> 4;
+export function createAccountReference(senderPrivateKeyBuffer, extendedPublicKeyBuffer, accountIndex = 0, version = 0) {
+    const accountSecretKeyBuffer = sha256hmac(senderPrivateKeyBuffer, extendedPublicKeyBuffer);
+    const accountSecretKeyBuffer32 = new Uint32Array(accountSecretKeyBuffer.buffer);
+    const accountSecretKey28 = accountSecretKeyBuffer32[0] >>> 4;
 
     const shortenedAccountBits = accountIndex & 0x0FFFFFFF;
     const versionBits = version << 28;
 
-    return versionBits | (AccountSecretKey28Buffer ^ shortenedAccountBits);
+    return versionBits | (accountSecretKey28 ^ shortenedAccountBits);
 }
