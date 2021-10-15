a1852bb31a9b235c91eb590c3d7b13b0519218e8
Alex Werner <alex@werner.fr>
2021-10-15 23:49:46 +0200

    fix: correctly decrypt shared key

--- src/SDK/Client/plugins/DashPay/methods/fetchEstablishedContacts.ts
+++ src/SDK/Client/plugins/DashPay/methods/fetchEstablishedContacts.ts
@@ -1,6 +1,5 @@
-import {Contact} from "../types/Contact";
-import {Profile} from "../types/Profile";
-import {HDPublicKey} from "@dashevo/dashcore-lib/typings/HDPublicKey";
+import { Contact } from "../types/Contact";
+import { HDPublicKey } from "@dashevo/dashcore-lib";
 
 /**
  * Fetch establish contact from a specific timestamp
@@ -44,36 +43,6 @@ export async function fetchEstablishedContacts(this: any, fromTimestamp = 0) {
                 .then(fetchAndPopulateIdentity)
                 .then(fetchAndPopulateDashPayProfile);
 
-            // The private key at snderKeyIndex of the sender and the public key at recipientKeyIndex of the recipient
-            console.log(contact);
-            const contactPublicKeys = contact.identity.publicKeys;
-            console.log({contactPublicKeys});
-            // const sendingSharedSecret = contact.receivedRequest.senderKeyIndex + contact.receivedRequest.recipientKeyIndex;
-            //The private key at the recipientKeyIndex of the recipient and the public key at senderKeyIndex of the sender
-            // const receivingSharedSecret = contact.sentRequest.senderKeyIndex + contact.sentRequest.recipientKeyIndex;
-            // For each contact we decrypt the publicKey
-            // const encryptedReceivingPublicKeyBuffer = 0;
-            // const encryptedPublicKeyBuffer = 0;
-            // const decryptedReceivingPublicKey = this.decryptPublicKey(encryptedPublicKeyBuffer, sharedSecret)
-            // const decryptedSendingPublicKey = this.decryptPublicKey(encryptedPublicKeyBuffer, sharedSecret)
-
-            // const contactXPublicKeyBuffer = Buffer.from(decryptedPublicKey, 'hex');
-            // const contactPublicKey = HDPublicKey.fromObject({
-                // parentFingerPrint: contactXPublicKeyBuffer.slice(0,4),
-                // chainCode: contactXPublicKeyBuffer.slice(4, 36),
-                // publicKey: contactXPublicKeyBuffer.slice(36, 69),
-                // network: 'testnet',
-                // depth: Buffer.alloc(1),
-                // childIndex: Buffer.alloc(4),
-            // })
-
-            contact.setAddresses({
-                // sending: [contactPublicKey.derive()],
-                // receiving: []
-                sending: ['yP8A3cbdxRtLRduy5mXDsBnJtMzHWs6ZXr'],
-                receiving: ['yP8A3cbdxRtLRduy5mXDsBnJtMzHWs6ZXr']
-            });
-
             infoFetchingPromises.push(infoFetchingPromise);
             establishedContacts.push(contact);
 
@@ -81,5 +50,54 @@ export async function fetchEstablishedContacts(this: any, fromTimestamp = 0) {
         }
     });
     await Promise.all(infoFetchingPromises);
+    const selfIdentitiesIds = this.storage.getIndexedIdentityIds(this.walletId);
+    const selfIdentitiesPrimises = [];
+    const selfIdentities = [];
+    selfIdentitiesIds.forEach((selfIdentitiesId) => {
+        // @ts-ignore
+        selfIdentitiesPrimises.push(this.platform.identities
+            .get(selfIdentitiesId)
+            // @ts-ignore
+            .then((id) => selfIdentities.push(id)
+            ));
+    });
+    await Promise.all(selfIdentitiesPrimises);
+
+    establishedContacts
+        .forEach((establishedContact) => {
+            const senderKey = establishedContact.identity.publicKeys[establishedContact.receivedRequest.data.senderKeyIndex];
+            // const receiverKey = selfIdentities[0].publicKeys[establishedContact.receivedRequest.data.recipientKeyIndex];
+
+            // @ts-ignore
+            const {privateKey} = this.identities.getIdentityHDKeyById(selfIdentities[0].getId().toString(), 0);
+
+            const sharedSecret = this.encryptSharedKey(privateKey.toBuffer(), senderKey.data);
+            const sentRequestEncryptedPublicKeyBuffer = establishedContact.sentRequest.data.encryptedPublicKey;
+            const sentDecryptedPublicKeyBuffer = Buffer.from(this.decryptPublicKey(sentRequestEncryptedPublicKeyBuffer, sharedSecret), 'hex');
+            const sentContactPublicKey = HDPublicKey.fromObject({
+                parentFingerPrint: sentDecryptedPublicKeyBuffer.slice(0, 4),
+                chainCode: sentDecryptedPublicKeyBuffer.slice(4, 36),
+                publicKey: sentDecryptedPublicKeyBuffer.slice(36, 69),
+                network: 'testnet',
+                depth: Buffer.from('07', 'hex'),
+                childIndex: Buffer.alloc(4),
+            })
+            const receivedRequestEncryptedPublicKeyBuffer = establishedContact.receivedRequest.data.encryptedPublicKey;
+            const receivedDecryptedPublicKeyBuffer = Buffer.from(this.decryptPublicKey(receivedRequestEncryptedPublicKeyBuffer, sharedSecret), 'hex');
+
+            const receivedContactPublicKey = HDPublicKey.fromObject({
+                parentFingerPrint: receivedDecryptedPublicKeyBuffer.slice(0, 4),
+                chainCode: receivedDecryptedPublicKeyBuffer.slice(4, 36),
+                publicKey: receivedDecryptedPublicKeyBuffer.slice(36, 69),
+                network: 'testnet',
+                depth: Buffer.from('07', 'hex'),
+                childIndex: Buffer.alloc(4),
+            })
+
+            establishedContact.setAddresses({
+                sending: [receivedContactPublicKey.deriveChild(0)],
+                receiving: [sentContactPublicKey.deriveChild(0)]
+            });
+        })
     return establishedContacts;
 }
